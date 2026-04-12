ec93f13624388ce61250dfd88c8586628417eaa5
Joern <misc@jokele.de>
2026-04-12 07:11:43 +0200

    Add git support

--- nvim/dot-config/nvim/lua/plugins/mini.lua
+++ nvim/dot-config/nvim/lua/plugins/mini.lua
@@ -2,6 +2,8 @@ vim.pack.add({
 	"https://github.com/nvim-mini/mini.starter",
 	"https://github.com/nvim-mini/mini.pairs",
 	"https://github.com/nvim-mini/mini.icons",
+	"https://github.com/nvim-mini/mini-git",
+	"https://github.com/nvim-mini/mini.diff",
 })
 
 -- Start screen
@@ -14,4 +16,20 @@ require("mini.pairs").setup()
 
 -- Icon provider, replace nvim_web_devicons
 require("mini.icons").setup()
-MiniIcons.mock_nvim_web_devicons()
+
+require("mini.git").setup()
+
+require("mini.diff").setup({
+	view = {
+		style = "sign",
+		signs = {
+			add = "+",
+			change = "|",
+			delete = "-",
+		},
+	},
+})
+
+vim.api.nvim_set_hl(0, "MiniDiffSignAdd", { fg = "#A6E22E", bold = true }) -- Lime Green
+vim.api.nvim_set_hl(0, "MiniDiffSignChange", { fg = "#66D9EF", bold = true }) -- Sky Blue
+vim.api.nvim_set_hl(0, "MiniDiffSignDelete", { fg = "#F92672", bold = true }) -- Electric Pink/Red
